bedb522bd8b89c953d7acbe581209d32e35f9e43
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-12 15:33:17 -0400

    Add multiple insert statements for payments table

--- tables.sql
+++ tables.sql
@@ -12,3 +12,23 @@ CREATE TABLE balances(
 CREATE TABLE large_table(
     random_int int
 )
+
+-- these aer 16 postgreSQL lines of code for inserting into the payments table the following values in SEQUENCE
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10);
+INSERT INTO payments VALUES ('clement', '2019-12-15', 10)
+
+-- more SQL commands;
